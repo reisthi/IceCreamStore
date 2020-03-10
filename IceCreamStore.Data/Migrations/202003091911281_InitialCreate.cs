e684b0dd40a66d1b4d3e1ed5bb4b1ea776f7daf7
Thiago dos Reis <tcdosreis@uams.edu>
2020-03-10 11:48:35 -0500

    .gitignore changes

---
+++ IceCreamStore.Data/Migrations/202003091911281_InitialCreate.cs
@@ -0,0 +1,27 @@
+﻿namespace IceCreamStore.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Restaurants",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 255),
+                        Location = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Restaurants");
+        }
+    }
+}
